84ad338d2f46b38149e971d3f1c6235bc4d99002
Bei <bei.bei@monash.edu>
2019-08-01 12:19:46 +1000

    Added examples of getting data from REDCap

---
+++ getREDCap.R
@@ -0,0 +1,22 @@
+## Examples of reading in data from REDCap
+
+library(data.table)
+library(REDCapR)
+
+## Store and read in API tokens from a severe location
+## I have two variables for the file I use:
+## proj: name of the project,
+## api: corresponding API token
+## This is helpful for accessing multiple projects
+
+redcap_api <-fread("~/location/REDCapAPI.csv")
+
+## Uncomment below to retrieve specific records or varibles
+## return_records <- c(1, 4)
+## desired_fields <- c("id", "name", "age")
+
+d <- as.data.table(redcap_read(redcap_uri = "https://redcap.cdms.org.au/api/",
+                               token = redcap_api[proj == "ProjectName", .(api)],
+                               ## fields = return_fields,
+                               ## records = return_records
+                               )$data)
