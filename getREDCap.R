5439d088f3ac05d9c711efc2b62ac00923ef6b9d
Bei <bei.bei@monash.edu>
2019-09-10 11:50:29 +1000

    Added keyring management of API security

--- getREDCap.R
+++ getREDCap.R
@@ -1,26 +1,27 @@
 ## Examples of reading in data from REDCap
 ## Need to install REDCapR from github, which will allow survey timestamp
-## to be download: 
+## to be download:
 ## install.packages("devtools") # Run this line if the 'devtools' package isn't installed already.
 ## devtools::install_github(repo="OuhscBbmc/REDCapR")
 
+## Use keyring to manage API security
+
 library(data.table)
 library(REDCapR)
+library(keyring)
 
-## Store and read in API tokens from a secure location
-## I have two variables for the file I use:
-## proj: name of the project,
-## api: corresponding API token
-## This is helpful for accessing multiple projects
+## Create API in macOS keychain
+## key_set("ProjectName", "Username")
+## Enter API as password when prompted
 
-redcap_api <-fread("~/location/REDCapAPI.csv")
+redcap_api <- key_get("ProjectName")
 
 ## Uncomment below to retrieve specific records or varibles
 ## return_records <- c(1, 4)
 ## return_fields <- c("id", "name", "age")
 
 d <- as.data.table(redcap_read(redcap_uri = "https://redcap.cdms.org.au/api/",
-                               token = as.character(redcap_api[proj == "ProjectName", .(api)]),
+                               token = redcap_api,
                                export_survey_fields = TRUE # needed to be TRUE for timestamp to be retrieved
                                ## fields = return_fields,
                                ## records = return_records
